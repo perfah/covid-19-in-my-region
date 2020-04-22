b396892e38a01f741bf8d809062d9d1cba317673
Per Fahlander <perfah@kth.se>
2020-04-22 22:07:44 +0200

    Fix test

--- covid_19_in_my_region/testing.py
+++ covid_19_in_my_region/testing.py
@@ -1,7 +1,9 @@
 import covid_19_in_my_region.backend as backend
 
+from covid_19_in_my_region import app
+
 
 def test_regions_exist():
-    dgg_file = backend.latest_csv_from_dgg()
-    regions = backend.construct_regions(dgg_file)
-    assert len(regions) > 0
+    with app.app_context():
+        regions = backend.get_regions()
+        assert len(regions) > 0
