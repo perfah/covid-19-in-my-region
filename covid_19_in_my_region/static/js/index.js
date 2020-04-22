e03c78b7d4be283d3c22d5f8c2accda7e5dae752
Joar Ekelund <joarek@kth.se>
2020-04-22 12:42:06 +0200

    client side AJAX request

--- covid_19_in_my_region/static/js/index.js
+++ covid_19_in_my_region/static/js/index.js
@@ -1,15 +1,22 @@
-window.addEventListener('load', fetchCounties);
-
-function fetchCounties() {
-
-}
-
 function request_data() {
-  TESTER = document.getElementById('tester');
-  Plotly.newPlot( TESTER, [{
-    x: [1, 2, 3, 4, 5],
-    y: [1, 2, 4, 8, 16] }], {
-      margin: { t: 0 }
-    }
-  );
+  var sel = document.getElementById('county-select').value;
+  console.log(sel);
+
+  fetch('/fetch_data?region=' + sel, {
+  method: 'GET',
+  headers:{
+    'Content-Type': 'application/json'
+  }
+  }).then(function(response){
+    response.json().then(function(data){
+      TESTER = document.getElementById('tester');
+      Plotly.newPlot( TESTER, [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16] }], {
+          margin: { t: 0 }
+        }
+      );
+    })
+  })
+
 }
